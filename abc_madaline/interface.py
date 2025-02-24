48c46c1d7ecfda8d4812a3be56500fbd6d477a53
LucasMoragas <moragaslucas08@gmail.com>
2025-02-24 16:57:07 -0300

    Feat: interface adjust

--- abc_madaline/interface.py
+++ abc_madaline/interface.py
@@ -7,6 +7,7 @@ class App:
         self.root = root
         self.master = Frame(master)
         self.master.pack()
+        
         self.header = Label(self.master, text="ABC Madaline")
         self.header['font'] = ('Verdana', '20')
         self.header.pack()
@@ -15,6 +16,7 @@ class App:
         self.matrix_container.pack()
         self.matrix = Frame(self.matrix_container)
         self.matrix.pack(side=LEFT)
+        
         self.checkboxes = []
         for i in range(8):
             row = []
@@ -25,12 +27,16 @@ class App:
                 row.append(var)
             self.checkboxes.append(row)
         
-        self.clear_button = Frame(master)
-        self.clear_button = Button(self.master, text="Clear", command=self.clear)
+        self.buttons = Frame(self.master)
+        self.buttons.pack(pady=10)
+        
+        self.clear_button = Button(self.buttons, text="Clear", command=self.clear)
         self.clear_button['font'] = ('Verdana', '10')
-        self.clear_button.pack()
+        self.clear_button.pack(side=LEFT, padx=5)
         
-        
+        self.train_button = Button(self.buttons, text="Train", command=self.train)
+        self.train_button['font'] = ('Verdana', '10')
+        self.train_button.pack(side=LEFT, padx=5)
         
     def clear(self):
         for row in self.checkboxes:
@@ -39,7 +45,12 @@ class App:
                 
     def get_matrix(self):
         return np.array([[checkbox.get() for checkbox in row] for row in self.checkboxes]).flatten()
-        
-        
-        
+    
+    def train(self):
+        pass
 
+if __name__ == "__main__":
+    root = Tk()
+    root.title("ABC Madaline")
+    app = App(root)
+    root.mainloop()
